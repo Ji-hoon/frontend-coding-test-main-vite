947d49a0b6742a10e58deba86f0b7c111ddc82ed
[SW7] 김지훈 <lanslot84@gmail.com>
2024-03-25 16:05:39 +0900

    Feat: type, constants, axios instance 추가

---
+++ src/global/constants.ts
@@ -0,0 +1,4 @@
+export const API_KEY =
+  "live_k77YJ1Sa3RsUfqEwbuKzrsevPSBW7iCoeeTZKSuj0ahl51TyYwbMXoLhVwwyIIvF";
+
+export const IMAGE_LIMIT_COUNT = 30;
---
+++ src/global/types.ts
@@ -0,0 +1,6 @@
+export type imageType = {
+  url: string;
+  width: number;
+  height: number;
+  id: string;
+};
