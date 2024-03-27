4916b31cf940c4e22c75236063b4181267316d61
[SW7] 김지훈 <lanslot84@gmail.com>
2024-03-27 11:14:11 +0900

    Feat: constants 업데이트

--- src/global/constants.ts
+++ src/global/constants.ts
@@ -1,11 +1,65 @@
 export const API_KEY =
   "live_k77YJ1Sa3RsUfqEwbuKzrsevPSBW7iCoeeTZKSuj0ahl51TyYwbMXoLhVwwyIIvF";
 
+export const TYPES = {
+  CONFIRM: "CONFIRM",
+  ADD: "ADD",
+  DELETE: "DELETE",
+};
+
 export const VALUES = {
   IMAGE_LIMIT_COUNT: 30,
   BREAKPOINT_XLARGE: 960,
   BREAKPOINT_LARGE: 500,
+  ANIMATION_TIMING_SHORTER: 200,
   ANIMATION_TIMING_SHORT: 300,
   ANIMATION_DELAY_50: 50,
   ANIMATION_DELAY_100: 100,
+};
+
+export const SIZES = {
+  XXS: 12,
+  XS: 14,
+  SM: 16,
+  MD: 18,
+  LG: 20,
+  XL: 24,
+  XXL: 36,
+  TITLE: 42,
+  MAX_WIDTH_INPUT: 240,
+  MAX_WIDTH_APP: 1200,
+};
+
+export const COLORS = {
+  BRAND_DEEP: "#7552F6",
+  BRAND_DEFAULT: "#7552F6",
+  BRAND_LIGHT: "#7552F6",
+  BASIC_WHITE: "#FFFFFF",
+  BASIC_BLACK: "#000000",
+  VARIATION_RED: "#EB5151",
+  VARIATION_YELLOW: "#FDE433",
+  VARIATION_PINK: "#F8C6F9",
+  VARIATION_PURPLE: "#D3B3FC",
+  VARIATION_BLUE: "#B3CBFC",
+  GRAY_00: "#F8F8F8",
+  GRAY_01: "#F5F5F5",
+  GRAY_02: "#E0E0E0",
+  GRAY_03: "#ADADAD",
+  GRAY_04: "#939393",
+  GRAY_05: "#848484",
+  GRAY_06: "#656565",
+  GRAY_07: "#575757",
+  GRAY_08: "#313131",
+  GRAY_09: "#242424",
+  GRAY_10: "#131313",
+  GRAY_01_OVERAY: "rgba(0, 0, 0, 0.08)",
+  GRAY_02_OVERAY: "rgba(0, 0, 0, 0.10)",
+  GRAY_03_OVERAY: "rgba(0, 0, 0, 0.12)",
+  GRAY_04_OVERAY: "rgba(0, 0, 0, 0.14)",
+  GRAY_05_OVERAY: "rgba(0, 0, 0, 0.16)",
+  GRAY_06_OVERAY: "rgba(0, 0, 0, 0.18)",
+  GRAY_07_OVERAY: "rgba(0, 0, 0, 0.2)",
+  GRAY_08_OVERAY: "rgba(0, 0, 0, 0.4)",
+  GRAY_09_OVERAY: "rgba(0, 0, 0, 0.75)",
+  GRAY_10_OVERAY: "rgba(0, 0, 0, 0.9)",
 };
